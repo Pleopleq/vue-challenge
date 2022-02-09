bb0362052b30548d89c1f512d6496375a7c80ea7
Felix Anducho <felix.anducho74@gmail.com>
2022-02-08 21:16:02 -0600

    Try env variables again

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -34,8 +34,7 @@ export default {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          Authorization:
-            "5bc95bf034d900548243a59e2296bd683729bd75057879fd0f877d3adc7d1db6bedbfccb47aca04e44ef28adf4c3e9e72afe2f2b295b3bf08e2a47ec75f9607d",
+          Authorization: process.env.VUE_APP_API_KEY,
         },
       })
         .then((response) => response.json())
